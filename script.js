e23f43e94ae499b1cb3517ec1ff07d5d09474f1e
R0XER5 <alitarikaydin20@gmail.com>
2021-11-14 20:07:03 +0300

    game is updated

--- script.js
+++ script.js
@@ -10,31 +10,44 @@ function jump(){
     },500);
     music();
 }
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 2s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+
+function changeColor (color){
+    if (color=="orange") {
+        document.getElementById("character").style.backgroundImage = "url('circle-24.ico')";
     }
-}, 10);
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.setAttribute("volume", 1);
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
+            if (color=="blue") {
+        document.getElementById("character").style.backgroundImage = "url('circle.ico')";
     }
-    this.stop = function(){
-        this.sound.pause();
-    }    
+                if (color=="red") {
+        document.getElementById("character").style.backgroundImage = "url('circle-24 (2).ico')";
+    }
 }
+
+function startGame() {
+    block.style.webkitAnimationPlayState="running";
+    var checkDead=setInterval(function(){
+        let characterTop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        let blockLeft=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        if (blockLeft<20 && blockLeft >- 20 && characterTop>=130) {
+            block.style.animation="none";
+            alert("Game Over (Score:"+Math.floor(counter/100)+")");
+            counter=0;
+            block.style.animation="block 2s infinite linear";
+        }
+        else{
+            counter++;
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+
+        }
+
+    },10);
+
+
+}
+
+function pauseGame () {
+    block.style.webkitAnimationPlayState="paused";
+
+}
+
+window.onload = pauseGame;
